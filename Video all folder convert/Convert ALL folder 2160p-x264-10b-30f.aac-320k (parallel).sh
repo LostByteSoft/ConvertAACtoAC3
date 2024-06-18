15fc056096ea630e5981ff45ca0982e5af43f592
LostByteSoft <mad_mike_41@hotmail.com>
2024-06-18 11:55:51 -0400

    Add files via upload

---
+++ Video all folder convert/Convert ALL folder 2160p-x264-10b-30f.aac-320k (parallel).sh
@@ -0,0 +1,482 @@
+#!/bin/bash
+#!/usr/bin/ffmpeg
+
+	start=$SECONDS
+	now=$(date +"%Y-%m-%d_%A_%H:%M:%S")
+
+echo -------------------------===== Start of bash ====-------------------------
+	#printf '\033[8;40;90t'		# will resize the window, if needed.
+	printf '\033[8;40;120t'		# will resize the window, if needed.
+	#printf '\033[8;40;130t'	# will resize the window, if needed.
+
+	echo
+	echo
+	me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+	echo "Running : $me"
+
+	red=`tput setaf 1`
+	green=`tput setaf 2`
+	yellow=`tput setaf 11`
+	blue=`tput setaf 12`
+	reset=`tput sgr0`
+
+	## General purposes variables. Watch before program to specific variables.
+	## All variables must be 0 or 1
+	autoquit=0	# autoquit anyway to script takes LESS than 2 min to complete. (0 or 1, change in conjoncture noquit=0)
+	debug=0		# test debug. (0 or 1 debug mode)
+	error=0		# test error. (0 or 1 make error)
+	noquit=0	# No quit after all operations. (0 or 1 noquit)
+
+	## Auto-generated variables.
+	random=$(shuf -i 4096-131072 -n 1)	# Used for temp folders. A big number hard to guess for security reasons.
+	random2=$RANDOM
+	part=0					# don't change this value. (0)
+	
+	echo
+	echo "Software lead-in. LostByteSoft ; https://github.com/LostByteSoft"
+	echo
+	echo "NEVER remove dual ## in front of lines. Theses are code annotations."
+	echo "You can test / remove single # for testing purpose."
+	echo
+	echo "Current time : $now"
+	echo
+	echo "Common variables, you can changes theses variables as you wish to test."
+	echo "Debug data : autoquit=$autoquit debug=$debug error=$error part=$part noquit=$noquit random=$random random2=$random2"
+	echo
+
+echo -------------------------========================-------------------------
+## Software name, what is this, version, informations.
+	echo "Software name: Convert FOLDER to"
+	echo
+	echo "What it does ?"
+	echo "Convert FOLDER to audio/video"
+	echo
+	echo "This is a MULTIPLE file conversion"
+	echo
+	echo "Read me for this file (and known bugs) :"
+	echo
+	echo "This software use theses softwares:"
+	echo
+	echo "Use ffmpeg https://ffmpeg.org/ffmpeg.html"
+	echo "Use Gnu Parallel https://www.gnu.org/software/parallel/"
+	echo
+	echo "Options https://trac.ffmpeg.org/wiki/Encode/H.264"
+	echo "4k demo HDR https://www.demolandia.net"
+	echo
+	echo "Informations : (EULA at the end of file, open in text.)"
+	echo "By LostByteSoft, no copyright or copyleft. https://github.com/LostByteSoft"
+	echo
+	echo "Don't hack paid software, free software exists and does the job better."
+	echo
+
+echo -------------------------========================-------------------------
+	echo Version compiled on : Also serves as a version
+	echo 2024-02-12_Monday_17:06:04
+echo -------------------------========================-------------------------
+echo "Color codes / Informations."
+	echo
+	echo  "${green}	████████████████     ALL OK / ACTIVE      ████████████████ ${reset}"
+	echo   "${blue}	████████████████      INFORMATION(S)      ████████████████ ${reset}"
+	echo "${yellow}	████████████████   ATTENTION / INACTIVE   ████████████████ ${reset}"
+	echo    "${red}	████████████████   FATAL ERROR / OFFLINE  ████████████████ ${reset}"
+	echo
+
+echo -------------------------========================-------------------------
+echo "Functions codes and color"
+	echo
+	echo 	"Function ${blue}█████${reset} Debug. Activate via source program debug=1."
+
+	debug()
+	if [ "$debug" -ge 1 ]; then
+		echo
+		echo "${blue}█████████████████████████████████ DEBUG ██████████████████████████████████${reset}"
+		echo
+		echo autoquit=$autoquit debug=$debug error=$error noquit=$quit count=$count part=$part random=$random
+		echo
+		echo file = $file
+		echo 
+		read -n 1 -s -r -p "Press any key to continue"
+		echo
+	fi
+	
+	if [ "$debug" -eq "1" ]; then
+		echo
+		echo "${blue}██████████████████████████████ DEBUG ACTIVATED ███████████████████████████${reset}"
+		echo
+		echo "Debug data : autoquit=$autoquit debug=$debug error=$error part=$part noquit=$noquit random=$random"
+		echo
+	fi
+
+	echo 	"Function ${red}█████${reset} Error detector. Errorlevel show error msg."
+
+	error()
+	if [ "$?" -ge 1 ]; then
+		part=$((part+1))
+		echo
+		echo "${red}█████████████████████████████████ ERROR $part █████████████████████████████████${reset}"
+		echo
+		echo "!!! ERROR was detected !!! Press ANY key to try to CONTINUE !!! Will probably exit !!!"
+		echo
+		debug=0
+		noquit=1
+		autoquit=0
+		read -n 1 -s -r -p "Press any key to CONTINUE"
+		echo
+	fi
+
+	echo 	"Function ${green}█████${reset} Auto Quit. If autoquit=1 will automaticly quit."
+	if [ "$autoquit" -eq "1" ]; then
+		echo
+		echo "${green}█████████████████████████ AUTO QUIT ACTIVATED █████████████████████████${reset}"
+		echo
+	fi
+
+	if [ "$automatic" -eq "1" ]; then
+		echo
+		echo "${yellow}███████████████████████████ AUTOMATIC ACTIVATED ████████████████████████${reset}"
+		echo
+	fi
+	echo
+
+echo -------------------------========================-------------------------
+	echo Check installed requirements !
+	echo
+if command -v ffmpeg >/dev/null 2>&1
+	then
+		echo "Ffmpeg installed continue."
+		dpkg -s ffmpeg | grep Version
+		echo "${green} ████████████████ OK ████████████████ ${reset}"
+		echo
+	else
+		echo "You don't have ' ffmpeg ' installed, now exit in 10 seconds."
+		echo "Add with : sudo apt-get install ffmpeg"
+		echo
+		echo "${red}████████████████ Dependency error ████████████████${reset}"
+		echo
+		read -n 1 -s -r -p "Press ENTER key to continue anyway (NOT a good idea) !"
+		echo
+	fi
+
+if command -v parallel >/dev/null 2>&1
+	then
+		echo "Parallel installed continue."
+		dpkg -s parallel | grep Version
+		echo "${green} ████████████████ OK ████████████████ ${reset}"
+		echo
+	else
+		echo "You don't have ' parallel ' installed, now exit in 10 seconds."
+		echo "Add with : sudo apt-get install parallel"
+		echo
+		echo "${red}████████████████ Dependency error ████████████████${reset}"
+		echo
+		read -n 1 -s -r -p "Press ENTER key to continue anyway (NOT a good idea) !"
+		echo
+	fi
+
+echo -------------------------========================-------------------------
+echo "All lowercase for convert... (NOT activated, remove both # to activate)"
+	echo
+	## This line put all lowercase FROM selected folder to the files names.
+	#echo "cd "$file" && find . -name '*.*' -exec sh -c ' a=$(echo "$0" | sed -r "s/([^.]*)\$/\L\1/"); [ "$a" != "$0" ] && mv "$0" "$a" ' {} \;"
+	#cd "$file" && find . -name '*.*' -exec sh -c ' a=$(echo "$0" | sed -r "s/([^.]*)\$/\L\1/"); [ "$a" != "$0" ] && mv "$0" "$a" ' {} \;
+
+echo -------------------------========================-------------------------
+echo "Names not supported / Informations."
+	echo
+	echo "${blue}	████████████████████████████████████████████████████████████████${reset}"
+	echo "		!!! NAMES starting with symbols are NOT SUPPORTED !!!"
+	echo "${blue}	████████████████████████████████████████████████████████████████${reset}"
+	echo
+
+echo -------------------------========================-------------------------
+echo "Select folder or filename using dialog !"
+	echo
+
+	#file="$(zenity --file-selection --filename=$HOME/ --title="Select a file, all format supported")"			## File select.
+	file=$(zenity --file-selection --filename=$HOME/ --title="Choose a directory to convert all file" --directory)	## Directory select.
+	## file="/$HOME/Pictures/"
+	## file="/$HOME/Downloads/"
+	## --file-filter="*.jpg *.gif"
+	## --file-filter='*[WwEeBbPp] | *[JjPpGg]' 
+
+	count=`ls -1 "$file" 2>/dev/null | wc -l`
+	echo Count : $count
+	echo "You have selected :"
+	echo "$file"
+	echo
+
+### file or folder
+	if test -z "$file"	## for cancel on zenity
+		then
+			echo "You click CANCEL !"
+			echo
+			echo -------------------------========================-------------------------
+			echo
+			echo "${yellow}█████████████████████ NO DATA TO PROCESS █████████████████████${reset}"
+			echo
+			read -n 1 -s -r -p "Press any key to EXIT"
+			echo
+			exit
+		fi
+
+	if [ "$count" -eq 0 ]	## for n files in directory
+		then
+			echo "You don't have selected a folder including files !"
+			echo
+			echo -------------------------========================-------------------------
+			echo
+			echo "${yellow}█████████████████████ NO DATA TO PROCESS █████████████████████${reset}"
+			echo
+			read -n 1 -s -r -p "Press any key to EXIT"
+			echo
+			exit
+		fi
+
+echo -------------------------========================-------------------------
+echo "Number of jobs processed concurrently at the same time ? (Refer as parallel CPU cores)"
+	echo
+
+	cpu=$(nproc)
+	defx=$(( cpu / 2 ))	## for audio files
+	defv=$(( cpu / 4 ))	## for video files
+	defi=$(( cpu * 2 ))	## for images files
+	defy=$(( cpu * 4 ))	## for images files
+	defz=$(( cpu * 8 ))	## for images files
+
+	## Put an # in front of "entry" to do an automatic choice.
+
+	entry=$(zenity --scale --value="1" --min-value="1" --max-value="$defi" --title "Convert files with Multi Cores Cpu" --text "How many cores do you want to use ? You have "$cpu" total cores !\n\n\tDefault suggested value is "$defv" for video.\n\n\tDefault suggested value is "$defv" for audio.\n\n\tDefault suggested value is ("$cpu" for xbrzscale) "$defi" for images.\n\n(1 to whatever core you want to use will work anyway !)")
+
+if test -z "$entry"
+	then
+		echo "Default value of "$cpu" (Safe value) will be used. Now continue."
+		entry=1
+		echo "You have selected : $entry"
+	else
+		echo "You have selected : $entry"
+	fi
+
+if [ "$entry" -ge $defi ]; then
+	echo
+	echo "${yellow}█████████████████████████ WARNING █████████████████████████${reset}"
+	echo
+	echo "!!! You have chosen a very high parallel work value, this may slow down the calculation rather than speed it up !!!"
+	echo
+	read -n 1 -s -r -p "Press any key to CONTINUE"
+	echo
+	fi
+	
+echo -------------------------========================-------------------------
+## Input_Directory_Output
+	echo "Input name, directory and output name : (Debug helper)"
+	echo
+
+## Set working path.
+	BASEDIR=$(dirname "$0")
+	echo Basedir : "$BASEDIR"
+	dir=$(pwd)
+
+## file or folder selected
+	echo "Working dir : "$dir""
+	echo Input file : "$file"
+	export VAR="$file"
+	echo
+
+## directory section
+	INPUT="$(dirname "${VAR}")"	
+	echo "Get the last Folder : ${INPUT##*/}"
+	echo Base directory : "$(dirname "${VAR}")"
+	echo Base name: "$(basename "${VAR}")"
+	echo
+
+## Output file name
+	name=`echo "$file" | rev | cut -f 2- -d '.' | rev` ## remove extension
+	echo "Output name ext : "$name""
+	name1=`echo "$(basename "${VAR}")" | rev | cut -f 2- -d '.' | rev` ## remove extension
+	echo "Output name bis : "$name1""
+	echo
+
+## Debug data
+	echo "Debug data : autoquit=$autoquit debug=$debug error=$error part=$part noquit=$noquit random=$random logs=$logs"
+	echo
+
+echo -------------------------========================-------------------------
+echo "The core/code program. Ffmpeg conversion"
+	echo
+
+part=$((part+1))
+echo "-------------------------===== Section $part =====-------------------------"
+echo Finding files...
+	#echo Delete /dev/shm/find.txt
+	rm "/dev/shm/find.txt" 2> /dev/null
+
+	## Easy way to add a file format, copy paste a new line.
+	echo "Will NOT find files in sub folders.... Remove -maxdepth 1 to search subfolders."
+
+	find "$file" -maxdepth 1 -iname '*.MP4'  >> "/dev/shm/find.txt"
+	find "$file" -maxdepth 1 -iname '*.MKV'  >> "/dev/shm/find.txt"
+	find "$file" -maxdepth 1 -iname '*.WEBM'  >> "/dev/shm/find.txt"
+	find "$file" -maxdepth 1 -iname '*.AVI'  >> "/dev/shm/find.txt"
+	echo
+
+part=$((part+1))
+echo "-------------------------===== Section $part =====-------------------------"
+echo show finded files... from /dev/shm/find.txt
+	echo	
+	cat "/dev/shm/find.txt"
+	echo
+	
+	if [ $logs -eq 1 ]; then
+		cat "/dev/shm/find.txt" >> /dev/shm/$random2.txt
+		fi
+
+part=$((part+1))
+echo "-------------------------===== Section $part =====-------------------------"
+
+	lines=$(wc -l < "/dev/shm/find.txt")
+	echo Finding finish, with file count : $lines
+	# echo $lines
+
+	if [ "$lines" -eq "0" ]; then
+		echo "You don't have selected a folder with files, now exit in 3 seconds."
+		echo -------------------------========================-------------------------
+		sleep 3
+		exit
+	fi
+
+part=$((part+1))
+echo "-------------------------===== Section $part =====-------------------------"
+
+##parallel -j $entry ffmpeg -i {} -vf scale=1280x2160:flags=lanczos,format=yuv420p -c:v libx264 -crf 22 -c:a aac -ac 2 -b:a 224k {.}.{2160p-SDR-x264-8b}.{aac-224k}.mkv ::: "$file"/*.*
+
+##parallel -j $entry ffmpeg -i {} -vf format=yuv420p10le -c:v libx264 -crf 22 -preset faster -tune fastdecode -c:a aac -b:a 320k {.}.{21600p-sdr-x264-10b}.{aac-320k}.mkv ::: "$file"/*.*
+
+	debug $?
+
+parallel -a "/dev/shm/find.txt" -j $entry ffmpeg -i {} -vf scale=3840x2160:flags=lanczos,format=yuv420p -r:v 30 -c:v libx264 -crf 22 -preset faster -tune fastdecode -c:a aac -ac 2 -b:a 320k {.}.{2160p-sdr-x264-10b-30f}.{aac-2.0-320k}.mkv
+
+	error $?
+	echo
+	
+	debug $?
+	rm "/dev/shm/find.txt" 2> /dev/null
+	
+echo -------------------------========================-------------------------
+## Software lead out
+
+	echo
+	echo "Debug data : autoquit=$autoquit debug=$debug error=$error part=$part noquit=$noquit random=$random random2=$random2 logs=$logs"
+	echo
+	echo "Finish... with numbers of actions : $part"
+	echo "This script take $(( SECONDS - start )) seconds to complete."
+	date=$(date -d@$(( SECONDS - start )) -u +%H:%M:%S)
+	echo "Time needed: $date"
+	now=$(date +"%Y-%m-%d_%A_%I:%M:%S")
+	echo "Current time : $now"
+	echo
+
+echo -------------------------========================-------------------------
+	echo
+	echo "If a script takes MORE than 120 seconds to complete it will ask"
+	echo "you to press ENTER to terminate."
+	echo
+	echo "If a script takes LESS than 120 seconds to complete it will auto"
+	echo "terminate after 10 seconds"
+	echo
+
+echo -------------------------===== End of Bash ======-------------------------
+## Exit, wait or auto-quit.
+
+	if [ "$error" -eq "1" ]; then
+		echo
+		echo "	${red}████████████████████████████████████████████${reset}"
+		echo "	${red}██                                        ██${reset}"
+		echo "	${red}██           ERROR ERROR ERROR            ██${reset}"
+		echo "	${red}██                                        ██${reset}"
+		echo "	${red}████████████████████████████████████████████${reset}"
+		echo
+		read -n 1 -s -r -p "Press ENTER key to exit !"
+		exit
+		fi
+
+	if [ "$noquit" -eq "1" ]; then
+		echo
+		echo "${blue}	█████████████████ NO exit activated ███████████████████${reset}"
+		echo
+		read -n 1 -s -r -p "Press ENTER key to exit !"
+		exit
+		fi
+
+	if [ "$autoquit" -eq "1" ]
+		then
+			echo
+			echo "${green}	███████████████ Finish, quit in 3 seconds █████████████████${reset}"
+			echo
+			sleep 2
+			echo
+		else
+		{
+			if [ "$debug" -eq "1" ]; then
+				echo
+				echo "${blue}		█████ DEBUG WAIT | Program finish. █████${reset}"
+				echo
+				echo "Debug data : autoquit=$autoquit debug=$debug error=$error part=$part noquit=$noquit random=$random"
+				echo
+				read -n 1 -s -r -p "Press ENTER key to continue !"
+				echo
+			fi
+		if [ $(( SECONDS - start )) -gt 120 ]
+			then
+				echo
+				echo "Script takes more than 120 seconds to complete."
+				echo
+				echo "${blue}	█████████████████████ Finish ███████████████████████${reset}"
+				echo
+				read -n 1 -s -r -p "Press ENTER key to exit !"
+				echo
+			else
+				echo
+				echo "Script takes less than 120 seconds to complete."
+				echo
+				echo "${green}	█████████████████████ Finish ███████████████████████${reset}"
+				echo
+				echo "Auto-quit in 3 sec. (You can press X)"
+				echo
+				sleep 3
+			fi
+		}
+		fi
+	exit
+
+## -----===== Start of eula =====-----
+
+	End-user license agreement (eula)
+
+ 	JUST DO WHAT THE F*** YOU WANT WITH THE PUBLIC LICENSE
+ 	
+ 	Version 3.1415926532 (January 2022)
+ 	
+ 	TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+    	
+	Everyone is permitted to copy and distribute verbatim or modified copies of
+ 	this license document.
+ 	
+ 	As is customary and in compliance with current global and interplanetary
+ 	regulations, the author of these pages disclaims all liability for the
+ 	consequences of the advice given here, in particular in the event of partial
+ 	or total destruction of the material, Loss of rights to the manufacturer
+ 	warranty, electrocution, drowning, divorce, civil war, the effects of radiation
+ 	due to atomic fission, unexpected tax recalls or encounters with
+ 	extraterrestrial beings elsewhere.
+ 	
+ 	YOU MUST ACCEPT THESES TERMS OR NOTHING WILL HAPPEN.
+ 	
+ 	LostByteSoft no copyright or copyleft we are in the center.
+ 	
+ 	You can send your request and your Christmas wishes to this address:
+ 	
+ 		Père Noël
+ 		Pôle Nord, Canada
+ 		H0H 0H0
+
+## -----===== End of file =====-----
